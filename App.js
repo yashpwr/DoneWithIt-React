de7f30082eba019f502a005c903dca401b3b02de
yash pawar <45911343+yashpwr@users.noreply.github.com>
2020-12-19 22:41:01 +0530

    Building the View Image Screen

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return <WelcomeScreen/>;
+  //return <WelcomeScreen/>;
+  return <ViewImageScreen/>;
 }
