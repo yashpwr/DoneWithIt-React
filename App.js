24724e4253d68f2debbfa375f1f3c616513313a5
yash pawar <45911343+yashpwr@users.noreply.github.com>
2020-12-20 19:36:24 +0530

    Building the Card Component

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { View } from 'react-native';
+import Card from './app/components/Card';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return <WelcomeScreen/>;
+  //return <WelcomeScreen/>;
   //return <ViewImageScreen/>;
+
+  return(
+    <View style={{
+      backgroundColor: "#f8f4f4",
+      padding: 20,
+      paddingTop: 100,
+    }}>
+
+    <Card 
+      title="Red jacket For Sale"
+      subTitle="$100"
+      image= {require("./app/assets/jacket.jpg")}
+    />
+    </View>
+  );
 }
