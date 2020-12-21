5169d181c1d5667067e5cd7f7464c632fc4ebda9
yashpwr <yashdvn@gmail.com>
2020-12-21 17:23:17 +0530

    Creating modal for Custom Picker

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
-import Screen from "./app/components/Screen"
+import Screen from "./app/components/Screen";
 import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,27 +1,39 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, TouchableWithoutFeedback, Modal, Button } from 'react-native';
 import {MaterialCommunityIcons} from "@expo/vector-icons";
 
 import defaultStyle from '../config/styles';
 import AppText from './AppText';
 
+
 function AppPicker({icon, placeholder, ...otherProps}) {
+
+   const [modalVisible, setModalVisible] =  useState(false);
+
     return (
-        <View style={styles.container}>
-            { icon && <MaterialCommunityIcons 
-                            name={icon} 
-                            size={20} 
-                            color={defaultStyle.colors.medium} 
-                            style={styles.icon} 
-                        />
-            }
-            <AppText style={styles.text}>{placeholder}</AppText>
-            <MaterialCommunityIcons 
-                            name="chevron-down" 
-                            size={20} 
-                            color={defaultStyle.colors.medium} 
-                        />
-        </View>
+        <>
+        <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+            <View style={styles.container}>
+                { icon && <MaterialCommunityIcons 
+                                name={icon} 
+                                size={20} 
+                                color={defaultStyle.colors.medium} 
+                                style={styles.icon} 
+                            />
+                }
+                <AppText style={styles.text}>{placeholder}</AppText>
+                <MaterialCommunityIcons 
+                                name="chevron-down" 
+                                size={20} 
+                                color={defaultStyle.colors.medium} 
+                            />
+            </View>
+        </TouchableWithoutFeedback>
+
+        <Modal visible={modalVisible} animationType="slide">
+                <Button title="Close" onPress={() => setModalVisible(false)} />
+        </Modal>
+        </>
     );
 }
 
