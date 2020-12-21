576ff2ab794223f4c537d01a23fd5a065474fe43
yashpwr <yashdvn@gmail.com>
2020-12-21 14:49:05 +0530

    building the listing screen

--- App.js
+++ App.js
@@ -9,13 +9,15 @@ import Screen from "./app/components/Screen"
 import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
+import ListingScreen from './app/screens/ListingScreen';
 
 export default function App() {
   //return <WelcomeScreen/>;
   //return <ViewImageScreen/>;
   //return <ListingDetailsScreen/>;
   //return <MessagesScreen/>;
-  return <AccountScreen/>;
+  //return <AccountScreen/>;
+  return <ListingScreen/>;
   
   // return(
   //   <Screen>
