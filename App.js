ff6cd7ba56aacc469465fb6e10832c22e61b2512
yashpwr <yashdvn@gmail.com>
2020-12-21 12:58:46 +0530

    building icon component

--- App.js
+++ App.js
@@ -5,25 +5,22 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from "./app/components/Screen"
+import Icon from './app/components/Icon';
 
 export default function App() {
   //return <WelcomeScreen/>;
   //return <ViewImageScreen/>;
   //return <ListingDetailsScreen/>;
-  return <MessagesScreen/>;
+  //return <MessagesScreen/>;
 
-  // return(
-  //   <View style={{
-  //     backgroundColor: "#f8f4f4",
-  //     padding: 20,
-  //     paddingTop: 100,
-  //   }}>
-
-  //   <Card 
-  //     title="Red jacket For Sale"
-  //     subTitle="$100"
-  //     image= {require("./app/assets/jacket.jpg")}
-  //   />
-  //   </View>
-  // );
+  return(
+    <Screen>
+      <Icon
+        name="email"
+        size={50}
+        backgroundColor="red"
+        iconColor="white" />
+    </Screen>
+  );
 }
