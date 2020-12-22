cb2842dca405402bd76c3d7f114e9098f8a1630f
yashpwr <yashdvn@gmail.com>
2020-12-22 11:24:26 +0530

    Building the login form

--- App.js
+++ App.js
@@ -2,27 +2,33 @@ import React, {useState} from 'react';
 import Screen from "./app/components/Screen";
 import AppTextInput from './app/components/AppTextInput';
 import AppPicker from './app/components/AppPicker';
+import LoginScreen from './app/screens/LoginScreen';
 
-const categories = [
-  { label: "Furniture", value: 1},
-  { label: "Clothing", value: 2},
-  { label: "Cameras", value: 3},
-  { label: "Laptops", value: 4},
-];
+// const categories = [
+//   { label: "Furniture", value: 1},
+//   { label: "Clothing", value: 2},
+//   { label: "Cameras", value: 3},
+//   { label: "Laptops", value: 4},
+// ];
 
 export default function App() {
 
-  const [Category, setCategory] = useState(categories[0]);
-
   return(
-    <Screen>
-      <AppPicker 
-          selectedItem={Category}
-          onSelectItem={item => setCategory(item)}
-          items={categories} 
-          icon="apps" 
-          placeholder="Category" />
-      <AppTextInput icon="email" placeholder="Email"/>
-    </Screen>
+    <LoginScreen/>
   );
+
+  //const [Category, setCategory] = useState(categories[0]);
+
+  // return(
+  //   <Screen>
+  //     <AppPicker
+  //         selectedItem={Category}
+  //         onSelectItem={item => setCategory(item)}
+  //         items={categories} 
+  //         icon="apps" 
+  //         placeholder="Category" />
+
+  //     <AppTextInput icon="email" placeholder="Email"/>
+  //   </Screen>
+  // );
 }
